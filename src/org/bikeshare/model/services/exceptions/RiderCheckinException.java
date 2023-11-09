3c3db03fa1fce9ab5dc09b7effea60b3c63cf037
kafargo <kevin.a.fargo@gmail.com>
2023-11-09 15:06:55 -0700

    Week 4 Initial Exception Setup and Branch Creation

---
+++ src/org/bikeshare/model/services/exceptions/RiderCheckinException.java
@@ -0,0 +1,9 @@
+package org.bikeshare.model.services.exceptions;
+
+public class RiderCheckinException extends Exception{
+
+    public RiderCheckinException(final String inMessage)
+    {
+        super(inMessage);
+    }
+}
